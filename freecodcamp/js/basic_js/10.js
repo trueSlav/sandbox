210796ede3fbccc5e0f9cdaa8cc10fe1cdf8f942
trueSlav <2001idercach@gmail.com>
2023-11-23 20:47:08 +0300

    A fcc task (7-10)

---
+++ freecodcamp/js/basic_js/10.js
@@ -0,0 +1,6 @@
+// Declare a Read-Only Variable with the const Keyword
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
---
+++ freecodcamp/js/basic_js/9.js
@@ -0,0 +1,4 @@
+// Explore Differences Between the var and let Keywords
+
+let catName = "Oliver";
+let catSound = "Meow!";
